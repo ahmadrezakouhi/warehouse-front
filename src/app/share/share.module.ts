56e40907452394605ac546f9da770db335b22ed0
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2023-09-22 22:29:49 +0330

    add header and sidbar component to share module

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidbarComponent } from './sidbar/sidbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    SidbarComponent,
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  
 })
 export class ShareModule { }
